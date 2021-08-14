c33d25908dc96fe92203fb57ce35e951e9f7cd24
maosan132 <maosan132@gmail.com>
2021-08-13 20:43:01 -0500

    Add component SearchBar

--- src/index.js
+++ src/index.js
@@ -4,10 +4,61 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const SearchBar = () => {
+  return (
+    <form>
+      <input type="text" placeholder="Search..."/>
+      {" "}
+      <p>
+        <input type="checkbox" />
+        <span style={{color: "green", fontSize: "smaller"}}>
+          Only show products in stock
+        </span>
+      </p>
+    </form>
+  )
+}
+
+const FilterableProductTable = props => {
+  const { products } = props;
+
+  return (
+    <div style={{fontFamily: "sans-serif"}}>
+      <SearchBar />
+      ProductTable here
+    </div>
+  )
+}
+
+const PRODUCTS = [
+	{	category: "Sporting Goods",
+			 price: "$49.99",
+		 stocked: true,
+				name: "Football" },
+	{	category: "Sporting Goods",
+			 price: "$9.99",
+		 stocked: true,
+				name: "Baseball" },
+	{	category: "Sporting Goods",
+			 price: "$29.99",
+		 stocked: false,
+				name: "Basketball" },
+	{	category: "Electronics",
+			 price: "$99.99",
+		 stocked: true,
+				name: "iPod Touch" },
+	{	category: "Electronics",
+			 price: "$399.99",
+		 stocked: false,
+				name: "iPhone 5" },
+	{	category: "Electronics",
+			 price: "$199.99",
+		 stocked: true,
+				name: "Nexus 7" }
+];
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <FilterableProductTable products={PRODUCTS}/>,
   document.getElementById('root')
 );
 
